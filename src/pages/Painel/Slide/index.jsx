e8ec870317a1d9ece05b80990c13f5bfcc8043ef
Samarión Júnior <samarion2@hotmail.com>
2022-07-19 10:29:22 -0300

    trabalhando no slide

--- src/pages/Painel/Slide/index.jsx
+++ src/pages/Painel/Slide/index.jsx
@@ -1,19 +1,27 @@
+import "./style.css"
+
 import React from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux"
 
 import * as PokemonActions from "./../../../store/actions/pokemon"
 
-const Painel = ({itens, setSelectedPokemon}) => (
+const Slide = ({itens, setSelectedPokemon}) => (
     
-    <div>
+    <div className="Slide">
         <h1>Slide</h1>
-        {itens?.map(item => (
-            <div key={item?.id}>
-                <p>{item?.name}</p>
-                <button onClick={() => setSelectedPokemon(item)}>Ver Mais</button>
+        <div className="Display">
+            <button className="Button">&lt;</button>
+            <div className="Scroll">
+                {itens?.map(item => (
+                    <div key={item?.id} className="Item">
+                        <p>{item?.name}</p>
+                        <button onClick={() => setSelectedPokemon(item)}>Ver Mais</button>
+                    </div>
+                ))}
             </div>
-        ))}
+            <button className="Button">&gt;</button>
+        </div>
     </div>
 )
 
@@ -23,5 +31,4 @@ const mapStateToProps = state => (
 
 const mapDispatchToProps = dispatch => bindActionCreators(PokemonActions, dispatch)
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(Painel)
+export default connect(mapStateToProps, mapDispatchToProps)(Slide)
