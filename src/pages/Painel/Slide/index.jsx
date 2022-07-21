1efd9ffd940d527a03e356c9842c83859b7dd1ed
Samarión Júnior <samarion2@hotmail.com>
2022-07-21 14:02:14 -0300

    Slide Finalizado

--- src/pages/Painel/Slide/index.jsx
+++ src/pages/Painel/Slide/index.jsx
@@ -6,13 +6,27 @@ import { bindActionCreators } from "redux"
 
 import * as PokemonActions from "./../../../store/actions/pokemon"
 
+const slideLeft = () => {
+    const slider = document.getElementById("Scroll")
+    console.log(slider.scrollLeft)
+    slider.scrollLeft = slider.scrollLeft - 100
+    console.log(slider.scrollLeft)
+}
+
+const slideRight = () => {
+    const slider = document.getElementById("Scroll")
+    console.log(slider.scrollLeft)
+    slider.scrollLeft = slider.scrollLeft + 100
+    console.log(slider.scrollLeft)
+}
+
 const Slide = ({itens, setSelectedPokemon}) => (
     
     <div className="Slide">
         <h1>Slide</h1>
-        <div className="Display">
-            <button className="Button">&lt;</button>
-            <div className="Scroll">
+        <div id="Slider" className="Slider">
+            <button className="Button" onClick={slideLeft}>&lt;</button>
+            <div id="Scroll" className="Scroll">
                 {itens?.map(item => (
                     <div key={item?.id} className="Item">
                         <p>{item?.name}</p>
@@ -20,7 +34,7 @@ const Slide = ({itens, setSelectedPokemon}) => (
                     </div>
                 ))}
             </div>
-            <button className="Button">&gt;</button>
+            <button className="Button" onClick={slideRight}>&gt;</button>
         </div>
     </div>
 )
