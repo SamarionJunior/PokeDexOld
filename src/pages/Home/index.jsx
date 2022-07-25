fc43bd0b7d364b6e0002216d14b3a2c7af734721
Samarión Júnior <samarion2@hotmail.com>
2022-07-25 18:22:39 -0300

    inicicando com style componet e tail wind

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,5 @@
+import tw from 'tailwind-styled-components'
+
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
@@ -6,6 +8,14 @@ import { Link } from "react-router-dom";
 import { bindActionCreators } from "redux";
 
 import * as PokemonActions from "../../store/actions/pokemon"
+
+const Grid = tw.div`
+    grid grid-cols-12 gap-4 bg-[#ff0000] font-[Roboto]
+`
+const Title = tw.h1`
+    col-span-12 text-center text-5xl
+    p-4 text-white
+`
 
 const Home = ({pokemons, setSelectedPokemon}) => {
     const [search, setSearch] = useState("")
@@ -23,22 +33,41 @@ const Home = ({pokemons, setSelectedPokemon}) => {
         }
     }, [search])
     return (
-        <div className="grid grid-cols-12 gap-4 justify-center">
-            <h1 className="col-span-12 text-center">Home</h1>
-            <div className="col-span-12">
-                <input className="m-[1em]" id="SearchPokemon" onChange={(e) => setSearch(String(e.target.value))} type="text" placeholder="Digite o nome do Pokemon!"/>
-                <input type="submit" placeholder="Pesquisar"/>
+        <Grid>
+            <Title>HOME</Title>
+            <div className="col-span-12 grid grid-cols-12 gap-0 h-screen">
+                <input
+                    className="
+                        col-start-4 col-span-4
+                        p-[1em] outline-0 bg-white
+                    "
+                    id="SearchPokemon" onChange={(e) => setSearch(String(e.target.value))} type="text" placeholder="Digite o nome do Pokemon!"/>
+                <button
+                    className="
+                        col-span-2
+                        p-[1em] outline-0 bg-white
+                    "
+                >
+                        Pesquisar
+                </button>
+                <div id="SearchBar" className="col-span-12 overflow-auto">
+                    {pokemonsFiltered.map(pokemonFiltered => (
+                        <Link key={pokemonFiltered.id} to="/painel" onClick={() => setSelectedPokemon(pokemonFiltered)}>
+                            {pokemonFiltered.name}
+                            <br/>
+                        </Link>
+                    ))}
+                </div>
             </div>
-            <div id="SearchBar" className="col-span-12 overflow-auto">
-                {pokemonsFiltered.map(pokemonFiltered => (
-                    <Link key={pokemonFiltered.id} to="/painel" onClick={() => setSelectedPokemon(pokemonFiltered)}>
-                        {pokemonFiltered.name}
-                        <br/>
-                    </Link>
-                ))}
-            </div>
-            <Link to="/gallery" className="col-span-12">Ver Todos</Link>
-        </div>
+            <Link to="/gallery"
+                className="
+                    col-start-6 col-span-2
+                    bg-white p-4 text-center
+                "
+            >
+                Ver Todos
+            </Link>
+        </Grid>
     )
 }
 
