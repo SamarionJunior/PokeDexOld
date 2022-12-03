d293bd21465a1c6363b2864a0fd595f31f406bea
Samarión Júnior <samarion2@hotmail.com>
2022-12-03 10:05:29 -0300

    Começando a estiliar

--- src/components/Toggle/index.jsx
+++ src/components/Toggle/index.jsx
@@ -1,12 +1,29 @@
-
+import "./style.css";
 
 import React from "react";
 
-const Toggle = ({title, children}) => (
-  <>
-    <h1>{title}</h1>
-    {children}
-  </>
+const close = () => {
+  const slider = document.getElementById("Scroll")
+  slider.scrollLeft = slider.scrollLeft - 100
+}
+
+const open = () => {
+  const slider = document.getElementById("ToggleContent")
+  // slider.style.display = "none"
+  if(slider){
+    console.log(slider.style)
+  }
+}
+
+const Toggle = props => (
+  <div className="Toggle">
+    <button className="ToggleButton" id="ToggleButton" onClick={open()}>
+      button
+    </button>
+    <div className="ToggleContent" id="ToggleContent">
+      {props.children}
+    </div>
+  </div>
 )
 
 export default Toggle;
