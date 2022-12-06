58816f9287344f852ff4a0c9a4a9ad0896cd09ec
Samarión Júnior <samarion2@hotmail.com>
2022-12-06 13:12:24 -0300

    Home Finalizada

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,4 @@
+import "../../style.css"
 import "./style.css"
 
 import React, { useEffect, useState } from "react";
@@ -10,12 +11,21 @@ import * as PokemonActions from "../../store/actions/pokemon"
 const Home = ({pokemons, setSelectedPokemon}) => {
     const [search, setSearch] = useState("")
     const [pokemonsFiltered, setPokemonsFiltered] = useState([])
+
+    const Blur = () => {
+        const DataList = document.getElementById("DataList");
+        DataList.style.display = "none"
+    }
+
+    const Focus = () => {
+        const DataList = document.getElementById("DataList");
+        DataList.style.display = "flex"
+    }
+
     useEffect(() => {
         const SearchInput = document.getElementById("SearchInput")
         const DataList = document.getElementById("DataList")
-        // const DataList = document.getElementById("DataList")
-        
-        const style = DataList.currentStyle || window.getComputedStyle(DataList);
+
         if(pokemonsFiltered.length !== 0){
             DataList.style.display = "flex"
         }else{
@@ -34,13 +44,17 @@ const Home = ({pokemons, setSelectedPokemon}) => {
     return (
         <div id="Home" className="Home">
             <div className="Content">
-                <h1 id="Title" className="Title">HOME</h1>
-                <div id="Searchbar" className="Searchbar">
+                <h1 id="Title" className="Title TextShadow">POKEDEX</h1>
+                <div id="Searchbar" className="Searchbar card-1">
                     <dir id="SearchSuggestions" className="SearchSuggestions">
-                        <input id="SearchInput" className="SearchInput" type="text" onChange={(e) => setSearch(String(e.target.value))} placeholder="Digite o nome do Pokemon!"/>
+                        <input  onBlur={Blur} onFocus={Focus} autoComplete="off" id="SearchInput" className="SearchInput" type="text" onChange={(e) => setSearch(String(e.target.value))} placeholder="Digite o nome do Pokemon!"/>
                         <div id="DataList" className="DataList">
                             {pokemonsFiltered.map(pokemonFiltered => (
-                                <Link key={pokemonFiltered.id} className="SearchItems" to="/painel" onClick={() => setSelectedPokemon(pokemonFiltered)}>
+                                <Link 
+                                    key={pokemonFiltered.id}
+                                    className="SearchItems zebrada"
+                                    to="/painel"
+                                    onClick={() => setSelectedPokemon(pokemonFiltered)}>
                                     {pokemonFiltered.name} <br/>
                                 </Link>
                             ))}
@@ -48,7 +62,7 @@ const Home = ({pokemons, setSelectedPokemon}) => {
                     </dir>
                     <button id="SearchButton" className="SearchButton">Pesquisar</button> {/*FALTA ESSA FUNÇÃO*/}
                 </div>
-                <Link className="GalleryLink" to="/gallery">Ver Todos</Link>
+                <Link className="GalleryLink card-1" to="/gallery">Ver Todos</Link>
             </div>
         </div>
     )
